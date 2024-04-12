87d8e0f36ae0a0a4b1cdcaf803bb2e0ef89f7e24
Pedro Boeing <boeingped@gmail.com>
2024-04-11 22:23:20 -0300

    Atividade 11/04

--- pasta/script.js
+++ pasta/script.js
@@ -1,15 +1,82 @@
-var link = location.search;
+function changePageTitle(title) {
+    document.title = title
+  }
+  
+function generateInfoSection(src, pokemonName) {
+    const h2 = document.createElement('h2')
+    h2.id = "info-pokemon-label"
+    h2.textContent = `Informações sobre ${pokemonName}`
+  
+    const img = document.querySelector('img')
+    img.src = src
+    img.alt = `Imagem do pokemon ${pokemonName}`
+  
+    const section = document.querySelector('#info-pokemon')
+  
+    section.appendChild(h2)
+    section.appendChild(img)
+}
+  
+async function getPokemonData(name) {
+    // fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    //   .then((fetchData) => {
+    //     return fetchData.json()
+    //   })
+    //   .then((jsonData) => generateInfoSection(jsonData.sprites.front_default, name))
+    //   .catch((error) => console.error(error))
+  
+    try {
+      const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  
+      const jsonData = await data.json()
 
-var t = new URLSearchParams(link);
+      generateInfoSection(jsonData.sprites.front_default, name)
 
-var r = t.get("evolucao");
+      var spritesArray = Object.values(sprites);
 
-document.title = "Página do " + r;
+      spritesArray = spritesArray.filter(item => typeof item === 'string');
 
-fetch("https://pokeapi.co/api/v2/pokemon/wartortle")
-.then(data => {
-    return data.json();
+      var currentIndex = 0;
+
+      exibirProximaImagem();
+  
+    } catch (error) {
+      console.error(error)
+    }
+}
+  
+function getSearchParams() {
+    // Early return -> Caso location search, não faz nada.
+    if (!location.search) {
+      return
+    }
+  
+    // URLSearchParams é uma classe que facilita a manipulação de query strings
+    const urlSearchParams = new URLSearchParams(location.search)
+  
+    // Pegando o valor do parâmetro name
+    const pokemonName = urlSearchParams.get('name')
+  
+    changePageTitle(`Pagina do ${pokemonName}`)
+    getPokemonData(pokemonName)
+}
+  
+document.addEventListener('DOMContentLoaded', function () {
+    getSearchParams()
 })
-.then(post => {
-    console.log(post.title);
-})
+
+function exibirProximaImagem() {
+    currentIndex++;
+
+    if (currentIndex >= spritesArray.length) {
+        currentIndex = 0;
+    }
+    
+    imagem.src = spritesArray[currentIndex];
+}
+
+
+
+
+
+imagem.addEventListener('click', exibirProximaImagem);
